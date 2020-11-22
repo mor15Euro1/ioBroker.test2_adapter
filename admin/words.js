c38f85610b43b0602b564a6a0dcd9cbe50fc3c62
mor15Euro1 <56317626+mor15Euro1@users.noreply.github.com>
2020-11-22 19:01:25 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"test2_adapter adapter settings": {
+		"en": "Adapter settings for test2_adapter",
+		"de": "Adaptereinstellungen für test2_adapter",
+		"ru": "Настройки адаптера для test2_adapter",
+		"pt": "Configurações do adaptador para test2_adapter",
+		"nl": "Adapterinstellingen voor test2_adapter",
+		"fr": "Paramètres d'adaptateur pour test2_adapter",
+		"it": "Impostazioni dell'adattatore per test2_adapter",
+		"es": "Ajustes del adaptador para test2_adapter",
+		"pl": "Ustawienia adaptera dla test2_adapter",
+		"zh-cn": "test2_adapter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
